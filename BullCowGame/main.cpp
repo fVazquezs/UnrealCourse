20ebac5329740f6b25a5c4b90a0d9f58e9428597
Fernando Vázquez <fernando.vz00@gmail.com>
2018-09-27 10:00:20 -0300

    just fix

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,22 +1,13 @@
-/* This is the console executable that makes the use of the BullCow class
-This acts as the view  in the MVC pattern, and is responsible for all
-user interaction . For game logic see FBullCow class.
-*/
-
 #include <iostream>
 #include <string>
-#include "FBullCowGame.h"
 
-using FText = std::string;
-using int32 = int32;
+using namespace std;
 
 void printIntro();
 void playGame();
-void printGuess(FText Guess);
-FText getGuess();
+void printGuess(string Guess);
+string getGuess();
 bool askToPlayAgain();
-
-FBullCowGame BCGame;//instantiate a new game 
 
 //the entry point of the application
 int main() {
@@ -28,58 +19,49 @@ int main() {
 }
 
 void playGame() {
-
-	BCGame.reset();
-	int32 MaxTries = BCGame.getMaxTries();
-
+	string Guess = "";
 	//loop for the number of turn asking for guesses
-	FText Guess = "";
-	for (int32 count = 0; count < MaxTries; count++)//TODO change for to while loop bc of the  validation
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 0; count < NUMBER_OF_TURNS; count++)
 	{
-		Guess = getGuess();//TODO implement the guess validation, make loop checking valid
-		
-		//submit the valid guess 
-		//print number of bulls and cows
-		
+		Guess = getGuess();
 		printGuess(Guess);
 	}
 	return;
-	//TODO implement if the game is won 
-	//TODO add game summary
 }
 
 //introduce the game
 void printIntro() {
-	constexpr int32 WORD_LENGTH = 9;
-	std::cout << "Welcome to Bulls and Cows!\n";
-	std::cout << "Can you guess the " << WORD_LENGTH << " isogram that it's in my mind.\n\n";
+	constexpr int WORD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows!\n";
+	cout << "Can you guess the " << WORD_LENGTH << " isogram that it's in my mind.\n\n";
 	return;
 }
 
 //get the guess 
-FText getGuess() {
-	int32 CurrentTry = BCGame.getCurrentTry();
+string getGuess() {
 
 	//get the guess from the player
-	FText Guess;
-	std::cout << "Give it your try number " << CurrentTry << ": ";
-	std::getline(std::cin, Guess);
+	string Guess;
+	cout << "Give it a try: ";
+	getline(cin, Guess);
 
 	return Guess;
 }
 
 //print the guess back
-void printGuess(FText Guess) {
-	std::cout << "This is your guess: " << Guess << std::endl << std::endl;
+void printGuess(string Guess) {
+	cout << "This is your guess: " << Guess << endl << endl;
 	return;
 }
 
 bool askToPlayAgain()
 {
-	std::cout << "Do you want to play again? [y/n]";
-	FText Response = "";
-	std::getline(std::cin, Response);
+	cout << "Do you want to play again? [y/n]";
+	string Response = "";
+	getline(cin, Response);
 
-	std::cout << "Is is y? "<< (Response[0] == 'y');
+	cout << "Is is y? "<< (Response[0] == 'y');
 	return (Response[0] == 'y') || (Response[0] == 'Y');
 }
+//just making a comment for my test
